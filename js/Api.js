bce22a1010243ff70321ca84ad6c999caec581b1
Jonathan Jallow <jonathan.jallow@hotmail.com>
2023-01-16 11:49:21 +0100

    Uppgift klar

---
+++ js/Api.js
@@ -0,0 +1,28 @@
+const field = document.getElementById('field');
+const container = document.getElementById('container');
+
+field.addEventListener('input', async function() {
+
+try {
+    const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=6417ec3a&s=' + field.value);
+    const data = await response.json();
+    const search = data.Search;
+    
+    container.innerHTML ="";
+   
+for (let data of search){
+    container.innerHTML += `<ul>
+
+   
+    <li>${data.Title}</li>
+    <li>${data.Year}</li>
+    <li>${data.imdbID}</li>
+    <li>${data.Type}</li>
+    <img src = ${data.Poster}>
+    </ul>`
+}
+
+} catch (error) {
+    console.log(error);
+}
+})
